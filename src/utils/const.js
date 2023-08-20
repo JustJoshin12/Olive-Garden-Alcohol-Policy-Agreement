a9f4cc71f8d408626543b2955ade6aa7126d1922
Joshua Smith <kazenostigma50@gmail.com>
2023-08-20 03:05:58 -0500

    added employee chart

---
+++ src/utils/const.js
@@ -0,0 +1,41 @@
+import signature from "../images/signature.png"
+
+
+
+ const employees = [
+    {
+        name:"Joshua Smith",
+        image:signature
+    },
+    {
+        name:"Joshua Smith",
+        image:signature
+    },
+    {
+        name:"Joshua Smith",
+        image:signature
+    },
+    {
+        name:"Joshua Smith",
+        image:signature
+    },
+    {
+        name:"Joshua Smith",
+        image:signature
+    },
+    {
+        name:"Joshua Smith",
+        image:signature
+    },
+    {
+        name:"Joshua Smith",
+        image:signature
+    },
+    {
+        name:"Joshua Smith",
+        image:signature
+    },
+
+]
+
+export default employees
